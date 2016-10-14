fcb61d2e75d9cf89d11210892dadbbeb153ff951
leshka <adorogov@ooobit.com>
2016-10-14 16:39:07 +0500

    Completed tests for Task 5.

--- HomeWork2/HomeWork2/BodyMassIndex.cs
+++ HomeWork2/HomeWork2/BodyMassIndex.cs
@@ -1,7 +1,48 @@
-﻿namespace HomeWork2
+﻿using System;
+
+namespace HomeWork2
 {
     public class BodyMassIndex
     {
-         
+        internal double GetIndex(double weight, double height)
+        {
+            if (height > 2.5) height = height/100;
+            return weight/height/height;
+        }
+
+        internal string Interprete(double index)
+        {
+            if (index < 16) return "Выраженный дефицит массы тела";
+            if (index >= 16 && index < 18.5) return "Недостаточная масса тела";
+            if (index >= 18.5 && index < 24.99) return "Ваша масса в норме";
+            if (index >= 25 && index < 30) return "Избыточная масса тела";
+            if (index >= 30 && index < 35) return "Ожирение первой степени";
+            if (index >= 35 && index < 40) return "Ожирение второй степени";
+            return "Ожирение третьей степени";
+        }
+
+        internal int CalculateNormalMass(double height)
+        {
+            if (height > 2.5) height = height / 100;
+            return (int) Math.Round(21.745*height*height);
+        }
+
+        internal string Advice(double weight, double height)
+        {
+            var index = GetIndex(weight, height);
+            var normalWeight = CalculateNormalMass(height);
+            var normalIndex = GetIndex(normalWeight, height);
+            var delta = index - normalIndex;
+
+            var advisedWeight = weight - normalWeight;
+
+            if (Math.Abs(delta) < 3.245) return "Так держать!";
+
+
+
+            return delta < 0
+                ? $"Вам не помешает набрать {normalWeight-weight} кг."
+                : $"Вам не помешает сбросить {weight-normalWeight} кг.";
+        }
     }
 }
---
+++ HomeWork2/HomeWork2Tests/BodyMassIndexTests.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.CodeDom;
+using HomeWork2;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HomeWork2Tests
+{
+    [TestClass]
+    public class BodyMassIndexTests
+    {
+        [TestMethod]
+        public void IndexIsCalculatedCorrectly()
+        {
+            var bmi = new BodyMassIndex();
+
+            var height = 1.7;
+            var weight = 60;
+            double index = bmi.GetIndex(weight, height);
+
+            Assert.AreEqual(20.8, Math.Round(index, 1));
+        }
+
+        [TestMethod]
+        public void IndexCorrectsCantimeters()
+        {
+            var bmi = new BodyMassIndex();
+
+            var height = 170;
+            var weight = 60;
+            double index = bmi.GetIndex(weight, height);
+
+            Assert.AreEqual(20.8, Math.Round(index, 1));
+        }
+
+        [TestMethod]
+        public void InterpreteReturnsRightStringsForDifferentBmis()
+        {
+            var bmi = new BodyMassIndex();
+
+            var normal = 20;
+            var lessThenNormal = 16.5;
+            var moreThenNormal = 31;
+
+            string normalResult = bmi.Interprete(normal);
+            string littleResult = bmi.Interprete(lessThenNormal);
+            string muchResult = bmi.Interprete(moreThenNormal);
+
+            Assert.AreEqual("Ваша масса в норме", normalResult);
+            Assert.AreEqual("Недостаточная масса тела", littleResult);
+            Assert.AreEqual("Ожирение первой степени", muchResult);
+        }
+
+        [TestMethod]
+        public void NormalMassReturns63For170()
+        {
+            var bmi = new BodyMassIndex();
+
+            var height = 1.7;
+            var index = 21.745;
+
+            int weight = bmi.CalculateNormalMass(height);
+
+            Assert.AreEqual(63, weight);
+        }
+
+        [TestMethod]
+        public void AdviceAdvises()
+        {
+            var bmi = new BodyMassIndex();
+
+            var weight = 100;
+            var w2 = 43;
+            var w3 = 60;
+            var height = 1.7;
+
+            string advice1 = bmi.Advice(weight, height);
+            string advice2 = bmi.Advice(w2, height);
+            string advice3 = bmi.Advice(w3, height);
+
+            Assert.AreEqual("Вам не помешает сбросить 37 кг.", advice1);
+            Assert.AreEqual("Вам не помешает набрать 20 кг.", advice2);
+            Assert.AreEqual("Так держать!", advice3);
+
+        }
+    }
+}
