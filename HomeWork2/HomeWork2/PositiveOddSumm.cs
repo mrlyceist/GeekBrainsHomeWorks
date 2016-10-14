a89f2238a2918b30e9365090ef7c473597e52f38
leshka <adorogov@ooobit.com>
2016-10-14 15:36:06 +0500

    Done tests for Task 3.

---
+++ HomeWork2/HomeWork2/PositiveOddSumm.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace HomeWork2
+{
+    public class PositiveOddSumm
+    {
+        internal List<int> FindOdds(List<int> numbers)
+        {
+            return numbers.Where(n => n%2 != 0 && n > 0).ToList();
+        }
+    }
+}
---
+++ HomeWork2/HomeWork2Tests/PositiveEvenSummTests.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using HomeWork2;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HomeWork2Tests
+{
+    [TestClass]
+    public class PositiveOddSummTests
+    {
+        [TestMethod]
+        public void FindOddsReturns1And3From1234()
+        {
+            var pos = new PositiveOddSumm();
+            List<int> numbers = new List<int> {1, 2, 3, 4};
+
+            List<int> foundOdds = pos.FindOdds(numbers);
+
+            Assert.AreEqual(2, foundOdds.Count);
+            Assert.IsTrue(foundOdds.Contains(1));
+            Assert.IsTrue(foundOdds.Contains(3));
+        }
+
+        [TestMethod]
+        public void FindOddsReturns1FromMinus1And1()
+        {
+            var pos = new PositiveOddSumm();
+            List<int> odds = new List<int> {-1, 1};
+
+            List<int> positives = pos.FindOdds(odds);
+
+            Assert.AreEqual(1, positives.Count);
+            Assert.IsTrue(positives.Contains(1));
+        }
+    }
+}
